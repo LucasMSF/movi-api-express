7e5143e40f0000b1d70fb322c07af9bc6301d251
LucasMSF <lucasgg003@gmail.com>
2022-06-29 01:30:17 -0300

    password from env

--- config/db.ts
+++ config/db.ts
@@ -3,7 +3,7 @@ import { Sequelize } from 'sequelize'
 
 const env = dotenv.config().parsed;
 
-export const sequelize = new Sequelize(env!.DB_DATA, env!.DB_USER, '', {
+export const sequelize = new Sequelize(env!.DB_DATA, env!.DB_USER, env!.DB_PASSWORD, {
     host: env!.DB_HOST,
     dialect: 'mysql'
 });
